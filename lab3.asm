65ddc6554003534f5db763bab9eb7d57bed027be
SaurianDavid <160608490+SaurianDavid@users.noreply.github.com>
2024-02-20 15:35:36 +0300

    Add files via upload

---
+++ lab3.asm
@@ -0,0 +1,15 @@
+; hello.asm 
+SECTION .data
+hellp: DB 'David Iskenderovd!'.10
+HelloLen: EQU $-hello
+SECTION .text
+GLOBAL_start
+_start:
+mov eax,4
+mov edx,1
+mov edx,hello
+mov edx,helloLen
+int 80h
+mov eax,1
+mov edx,0
+int 80h
